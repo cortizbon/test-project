ff7cc8f77038589d1e99e19742e70b68706c22f2
Carlos Ortiz <carlosortiz@192.168.0.9>
2024-01-13 11:39:41 -0500

    new change 2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ data = np.random.normal(0, 1, size=(1000, 4))
 df = pd.DataFrame(data=data,
                   columns=['col1', 'col2', 'col3', 'col4'])
 
-fig, ax = plt.subplots(1, 3, figsize=(2, 6))
+fig, ax = plt.subplots(1, 3, figsize=(6, 2))
 
 ax[0].hist(df['col1'], color='orange')
 ax[1].scatter(df['col1'], df['col2'], color='red')
@@ -23,7 +23,7 @@ fig.tight_layout()
 st.pyplot(fig)
 
 if st.button("Clic aquí"):
-    fig, ax = plt.subplots(1, 2)
+    fig, ax = plt.subplots(1, 2, figsize=(4, 2))
     ax[0].hist(df['col3'], bins=50)
     ax[1].scatter(df['col1'], df['col4'])
     st.pyplot(fig)
