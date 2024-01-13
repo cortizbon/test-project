6178bf5c90dcff0e8b351d3169f3ed9dd0d17d3e
Carlos Ortiz <carlosortiz@192.168.0.9>
2024-01-13 11:42:19 -0500

    changing plots

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout="wide")
 
 st.title("Aplicación web")
 
-data = np.random.normal(0, 1, size=(1000, 4))
+np.random.seed(12345)  # sembrar una semilla
 
-df = pd.DataFrame(data=data,
-                  columns=['col1', 'col2', 'col3', 'col4'])
+data = np.random.normal(
+    0, 1, size=(1000, 4)
+)  # generar una matriz de números que siguen una distribución normal
+
+df = pd.DataFrame(
+    data=data, columns=["col1", "col2", "col3", "col4"]
+)  # generar una tabla con los datos anteriores
 
 fig, ax = plt.subplots(1, 3, figsize=(6, 2))
 
-ax[0].hist(df['col1'], color='orange')
-ax[1].scatter(df['col1'], df['col2'], color='red')
-ax[2].hist(df['col3'], color='darkred')
+ax[0].hist(df["col1"], color="orange")
+ax[1].scatter(df["col1"], df["col2"], color="red")
+ax[2].hist(df["col3"], color="darkred")
 
 fig.tight_layout()
 
@@ -24,7 +29,6 @@ st.pyplot(fig)
 
 if st.button("Clic aquí"):
     fig, ax = plt.subplots(1, 2, figsize=(4, 2))
-    ax[0].hist(df['col3'], bins=50)
-    ax[1].scatter(df['col1'], df['col4'])
+    ax[0].hist(df["col3"], bins=50)
+    ax[1].scatter(df["col1"], df["col4"])
     st.pyplot(fig)
-
