456c4cf7c4b3d0c81519ad6a77a5722ddf9c6e90
Carlos Ortiz <carlosortiz@192.168.0.9>
2024-01-13 11:28:47 -0500

    add plots

---
+++ app.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+data = np.random.normal(0, 1, size=(50, 4))
+
+df = pd.DataFrame(data=data,
+                  columns=['col1', 'col2', 'col3', 'col4'])
+
+fig, ax = plt.subplots(1, 3)
+
+ax[0].hist(df['col1'])
+ax[1].scatter(df['col1'], df['col2'])
+ax[2].hist(df['col3'])
+
+st.pyplot(fig)
+
