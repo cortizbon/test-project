26cf7ccfc05d32e9faefd4173ccfbcda0ff8a14b
Carlos Ortiz <carlosortiz@192.168.0.9>
2024-01-13 11:29:49 -0500

    adding streamlit lines

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
+st.title("Aplicación web")
 
 data = np.random.normal(0, 1, size=(50, 4))
 
@@ -14,6 +15,8 @@ fig, ax = plt.subplots(1, 3)
 ax[0].hist(df['col1'])
 ax[1].scatter(df['col1'], df['col2'])
 ax[2].hist(df['col3'])
+
+fig.tight_layout()
 
 st.pyplot(fig)
 
